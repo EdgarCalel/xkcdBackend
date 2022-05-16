758ccb032550f8447d6f55d665ce8a4b0ad9a84b
EdgarCalel <edgar_kll@hotmail.com>
2022-05-16 00:55:18 -0600

    structure the frontend de cpage the comic xkcd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./src/app.js');
-
+//server
   server.listen( process.env.PORT || 4001, () => {
     console.log('%s listening at 4001'); // eslint-disable-line no-console
   });
