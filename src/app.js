f218e1aba3e1f6f7fc4b585e055e5af825766728
EdgarCalel <edgar_kll@hotmail.com>
2022-05-16 00:47:45 -0600

    structure the frontend de cpage the comic xkcd

--- src/app.js
+++ src/app.js
@@ -4,18 +4,14 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const { CORS_URL }= process.env
-
 const server = express();
-
 server.name = 'API';
-
 server.use(express.urlencoded({ extended: false, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', CORS_URL);
-
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
